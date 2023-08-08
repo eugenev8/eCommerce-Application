4fb641eda29c50ea4ecacddc230c3a039bdef39e
Zhenya Vasilev <vasilevz195@gmail.com>
2023-08-08 15:54:54 +0600

    feat: add basic routing

--- src/App.test.tsx
+++
@@ -1,5 +0,0 @@
-import App from './App';
-
-test('App not undefined', () => {
-  expect(App).not.toBeUndefined();
-});
---
+++ src/components/navigation/NavigationTypes.ts
@@ -0,0 +1,6 @@
+export interface NavLinksProps {
+  route: {
+    path: string;
+    name: string;
+  };
+}
