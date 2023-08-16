cca6282b73b8af2e884e1e1a0d760664e48014cd
Zhenya Vasilev <vasilevz195@gmail.com>
2023-08-16 17:09:03 +0600

    refactor: changed login form to use separate components

--- src/components/forms/login/LoginForm.tsx
+++ src/components/forms/login/LoginForm.tsx
@@ -1,9 +1,10 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import './LoginForm.css';
 
-import React, { useState } from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
+import EmailInput from '../inputs/EmailInput';
+import PasswordInput from '../inputs/PasswordInput';
 
 interface LoginFormValues {
   email: string;
@@ -31,11 +32,9 @@ const validationSchema = Yup.object({
 });
 
 function LoginForm() {
-  const [showPassword, setShowPassword] = useState(false);
-
   const handleSubmit = (values: LoginFormValues) => {
     // Handle login logic here
-    alert(values);
+    alert(JSON.stringify(values, null, 2));
   };
 
   return (
@@ -49,47 +48,22 @@ function LoginForm() {
         onSubmit={handleSubmit}
       >
         <Form className="loginForm__formContainer">
-          <div className="loginForm__inputContainer loginForm__email">
-            <label className="loginForm__label" htmlFor="email">
-              Email
-            </label>
-            <Field
-              placeholder="Type your email"
-              type="email"
-              id="email"
-              name="email"
-              aria-describedby="email-error"
-              className="loginForm__input"
-            />
-            <div className="loginForm__errorMessage">
-              <ErrorMessage name="email" />
-            </div>
-          </div>
-          <div className="loginForm__inputContainer loginForm__password">
-            <label className="loginForm__label" htmlFor="password">
-              Password
-            </label>
-            <div className="loginForm__passwordWrapper">
-              <Field
-                placeholder="Type your password"
-                type={showPassword ? 'text' : 'password'}
-                id="password"
-                name="password"
-                aria-describedby="password-error"
-                className="loginForm__input loginForm__passwordInput"
-              />
-              <button
-                className="loginForm__passwordToggle"
-                type="button"
-                onClick={() => setShowPassword(!showPassword)}
-              >
-                {showPassword ? 'Hide' : 'Show'}
-              </button>
-            </div>
-            <div className="loginForm__errorMessage">
-              <ErrorMessage name="password" />
-            </div>
-          </div>
+          <EmailInput
+            labelText="Email"
+            placeholder="Type your email"
+            id="email"
+            name="email"
+            parentClassName="loginForm"
+          />
+
+          <PasswordInput
+            labelText="Password"
+            placeholder="Type your password"
+            id="password"
+            name="password"
+            parentClassName="loginForm"
+          />
+
           <button type="submit" className="loginForm__submitButton">
             Login
           </button>
