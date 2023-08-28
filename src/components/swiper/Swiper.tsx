35343c7794277823c3a9606fce9063f76f8cf8f7
Zhenya Vasilev <vasilevz195@gmail.com>
2023-08-28 23:16:15 +0600

    feat: add Swiper

---
+++ src/components/swiper/Swiper.tsx
@@ -0,0 +1,43 @@
+import { A11y, Navigation, Pagination, Scrollbar } from 'swiper/modules';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css/bundle';
+import FlexContainer from '../containers/FlexContainer';
+
+interface SwiperProps {
+  imageUrlArray: string[];
+}
+
+export default function SwiperContainer({ imageUrlArray }: SwiperProps) {
+  return (
+    <Swiper
+      // install Swiper modules
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      centeredSlides
+      slidesPerView={1}
+      navigation
+      pagination={{ clickable: true }}
+      scrollbar={{ draggable: true }}
+    >
+      {imageUrlArray.map((url) => {
+        return (
+          <SwiperSlide key={url}>
+            <FlexContainer
+              style={{
+                height: '100%',
+                width: '100%',
+                justifyContent: 'center',
+                alignItems: 'center',
+                userSelect: 'none',
+                overflow: 'hidden',
+              }}
+            >
+              <img style={{ maxInlineSize: '100%', blockSize: 'auto', objectFit: 'contain' }} src={url} alt="" />
+            </FlexContainer>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
+}
