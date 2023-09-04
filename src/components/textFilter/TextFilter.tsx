c4ca7e3e5e8621a8af9e07a11ad492bbf3a886de
Zhenya Vasilev <vasilevz195@gmail.com>
2023-09-04 20:53:46 +0600

    feat: add text filter keypress handler, change to update only on filter press

--- src/components/textFilter/TextFilter.tsx
+++ src/components/textFilter/TextFilter.tsx
@@ -1,14 +1,25 @@
+import React from 'react';
 import styles from './TextFilter.module.scss';
 import { useAppDispatch, useAppSelector } from '../../hooks/redux';
 import { querySlice } from '../../reducers/QuerySlice';
 
-export default function TextFilter() {
+interface TextFilterProps {
+  onEnterKeyPress: () => void; // Callback function to handle Enter key press
+}
+
+export default function TextFilter({ onEnterKeyPress }: TextFilterProps) {
   const dispatch = useAppDispatch();
 
   const searchState = useAppSelector((state) => state.queryReducer.search);
 
   function handleChangeSearchText(searchText: string) {
     dispatch(querySlice.actions.changeSearchText(searchText));
+  }
+
+  function handleKeyPress(event: React.KeyboardEvent<HTMLInputElement>) {
+    if (event.key === 'Enter') {
+      onEnterKeyPress();
+    }
   }
 
   return (
@@ -20,6 +31,7 @@ export default function TextFilter() {
         placeholder="Enter key search"
         value={searchState}
         onChange={(e) => handleChangeSearchText(e.target.value)}
+        onKeyDown={handleKeyPress}
       />
     </div>
   );
