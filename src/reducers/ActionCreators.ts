9b74f29cceab87652cd9fa7071da2cd0e7415f9c
Trachenko Viktor <trachenko.v.a@gmail.com>
2023-09-10 00:30:10 +0300

    feat: add cartSlice, actions: create customer cart, get active cart

--- src/reducers/ActionCreators.ts
+++ src/reducers/ActionCreators.ts
@@ -1,5 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import {
+  Cart,
+  CartDraft,
   Category,
   Customer,
   CustomerDraft,
@@ -13,6 +15,8 @@ import toaster from '../services/toaster';
 import getErrorMessageForUser from '../utils/getErrorMessageForUser';
 import tokenStore from '../sdk/tokenStore';
 import { authSlice } from './AuthSlice';
+
+const CURRENCY = 'USD';
 
 const loginWithPassword = createAsyncThunk<Customer, CustomerSignin, { rejectValue: string }>(
   'customer/loginWithPassword',
@@ -126,4 +130,49 @@ const getCategories = createAsyncThunk<Category[], void, { rejectValue: string }
   }
 );
 
-export { loginWithPassword, signupCustomer, changeCustomerPassword, updateCustomerData, getCategories };
+const createCustomerCart = createAsyncThunk<Cart, string, { rejectValue: string }>(
+  'cart/createCustomerCart',
+  async (sku: string, { rejectWithValue, dispatch }) => {
+    try {
+      if (!apiRoots.TokenFlow) {
+        return rejectWithValue('Error with token flow');
+      }
+      const cartDraft: CartDraft = { currency: CURRENCY, lineItems: [{ sku }] };
+      const cartRes = await apiRoots.TokenFlow.me().carts().post({ body: cartDraft }).execute();
+      return cartRes.body;
+    } catch (error) {
+      const errorMessage =
+        error instanceof Error ? getErrorMessageForUser(error.message) : getErrorMessageForUser('Unknown error');
+      dispatch(authSlice.actions.authError(errorMessage));
+      return rejectWithValue(errorMessage);
+    }
+  }
+);
+
+const getCustomerCart = createAsyncThunk<Cart, void, { rejectValue: string }>(
+  'cart/getCustomerCart',
+  async (_, { rejectWithValue, dispatch }) => {
+    try {
+      if (!apiRoots.TokenFlow) {
+        return rejectWithValue('Error with token flow');
+      }
+      const cartRes = await apiRoots.TokenFlow.me().activeCart().get().execute();
+      return cartRes.body;
+    } catch (error) {
+      const errorMessage =
+        error instanceof Error ? getErrorMessageForUser(error.message) : getErrorMessageForUser('Unknown error');
+      dispatch(authSlice.actions.authError(errorMessage));
+      return rejectWithValue(errorMessage);
+    }
+  }
+);
+
+export {
+  loginWithPassword,
+  signupCustomer,
+  changeCustomerPassword,
+  updateCustomerData,
+  getCategories,
+  createCustomerCart,
+  getCustomerCart,
+};
