b17e05fd2de74aa06276cc30169735a07d19005a
mgg43er <mgg43er@gmail.com>
2023-09-15 13:20:37 +0300

    fix(elements): fix adaptive basket item
    refactor(elements): refactor basket adaptive

--- src/pages/basket/renderLineItems.tsx
+++ src/pages/basket/renderLineItems.tsx
@@ -16,12 +16,16 @@ const renderLineItems = (lineItems: LineItem[]) => {
       <div className={styles.basket__item__img}>
         <img src={item?.variant?.images?.[0]?.url} alt={item.name[`en-US`]} />
       </div>
-      <div>{`${item.name['en-US']} ${item.variant?.attributes?.map((a) => `${a.name}: ${a.value}`).join(' ')}`}</div>
-      <div key="price">{item.price.value.centAmount / 10 ** item.price.value.fractionDigits}</div>
+      <div className={styles.specs}>{`${item.name['en-US']} ${item.variant?.attributes
+        ?.map((a) => `${a.name}: ${a.value}`)
+        .join(' ')}`}</div>
+      <div className={styles.price} key="price">
+        {`$${item.price.value.centAmount / 10 ** item.price.value.fractionDigits}`}
+      </div>
       <div className={styles.quantity}>
         <input key="quantity" type="number" placeholder={String(item.state[0].quantity)} min={0} />
       </div>
-      <div>{item.price.value.centAmount / 10 ** item.price.value.fractionDigits}</div>
+      <div className={styles.subtotal}>{`$${item.price.value.centAmount / 10 ** item.price.value.fractionDigits}`}</div>
       <div className={styles.button__container}>
         <button type="button" className={styles.button} onClick={() => removeItemHandler()}>
           <img src={deletesvg} alt="delete" />
