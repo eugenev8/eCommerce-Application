fe17607f9a78c6c4adab770ce5ade32328193c28
Trachenko Viktor <trachenko.v.a@gmail.com>
2023-09-17 16:05:46 +0300

    refactor: migrate change personal data method

--- src/components/forms/edit/EditCustomerForm.tsx
+++ src/components/forms/edit/EditCustomerForm.tsx
@@ -1,18 +1,15 @@
 import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
 
-import { Customer, MyCustomerUpdate } from '@commercetools/platform-sdk';
 import { useState } from 'react';
 import CommonInput from '../inputs/CommonInput';
 import { AgeValidation, EmailValidation, FirstNameValidation, LastNameValidation } from '../CommonValidation';
 import Button from '../../buttons/Buttons';
 import FlexContainer from '../../containers/FlexContainer';
-import { useAppDispatch } from '../../../hooks/redux';
-import { updateCustomerData } from '../../../reducers/ActionCreators/Customer';
+import useManageCustomer, { CustomerPersonalData } from '../../../hooks/useManageCustomer';
 import toaster from '../../../services/toaster';
 
 interface EditCustomerFormProps {
-  customer: Customer;
   onSave: (isUpdated: boolean) => void;
 }
 
@@ -23,70 +20,31 @@ const validationSchema = Yup.object({
   dateOfBirth: AgeValidation,
 });
 
-export default function EditCustomerSmallForm({ customer, onSave }: EditCustomerFormProps) {
+export default function EditCustomerSmallForm({ onSave }: EditCustomerFormProps) {
   const [isSubmitting, setSubmitting] = useState(false);
+  const { customer, updatePersonalData: updateCustomerPersonalData } = useManageCustomer();
 
-  const dispatch = useAppDispatch();
+  if (!customer) return null;
 
-  const initialValues = {
+  const initialValues: CustomerPersonalData = {
     firstName: customer.firstName,
     lastName: customer.lastName,
     email: customer.email,
     dateOfBirth: customer.dateOfBirth,
   };
 
-  function createMyCustomerUpdate(values: Pick<Customer, 'firstName' | 'lastName' | 'email' | 'dateOfBirth'>) {
-    const updates: MyCustomerUpdate = { version: customer.version, actions: [] };
-    if (customer.firstName !== values.firstName) {
-      updates.actions.push({
-        firstName: values.firstName,
-        action: 'setFirstName',
-      });
-    }
-    if (customer.lastName !== values.lastName) {
-      updates.actions.push({
-        lastName: values.lastName,
-        action: 'setLastName',
-      });
-    }
-    if (customer.email !== values.email) {
-      updates.actions.push({
-        email: values.email,
-        action: 'changeEmail',
-      });
-    }
-    if (customer.dateOfBirth !== values.dateOfBirth) {
-      updates.actions.push({
-        dateOfBirth: values.dateOfBirth,
-        action: 'setDateOfBirth',
-      });
-    }
-
-    return updates;
-  }
-
-  const handleSubmit = (values: Pick<Customer, 'firstName' | 'lastName' | 'email' | 'dateOfBirth'>) => {
+  const handleSubmit = (values: CustomerPersonalData) => {
     setSubmitting(true);
-
-    const updates = createMyCustomerUpdate(values);
-    if (!updates.actions.length) {
-      toaster.showError('Nothing to change!');
-      setSubmitting(false);
-      return;
-    }
-    dispatch(updateCustomerData(updates))
-      .then((payloadAction) => {
-        if (payloadAction.type.includes('rejected')) {
-          toaster.showError('Something went wrong!');
-          onSave(false);
-        } else {
-          toaster.showSuccess('Personal data changed successfully!');
-          onSave(true);
-        }
+    updateCustomerPersonalData(values)
+      .then(() => {
+        toaster.showSuccess('Personal data changed successfully!');
+        onSave(true);
       })
-      .finally(() => {
-        setSubmitting(false);
-      });
+      .catch((error) => {
+        toaster.showError(error.message);
+        onSave(false);
+      })
+      .finally(() => setSubmitting(false));
   };
 
   return (
--- src/hooks/useManageCustomer.ts
+++ src/hooks/useManageCustomer.ts
@@ -1,12 +1,58 @@
-import { CustomerDraft, CustomerSignin } from '@commercetools/platform-sdk';
-import { useAppDispatch } from './redux';
-import { loginCustomer, signupCustomer } from '../reducers/ActionCreators/Customer';
+import { CustomerDraft, CustomerSignin, MyCustomerUpdate } from '@commercetools/platform-sdk';
+import { useAppDispatch, useAppSelector } from './redux';
+import { loginCustomer, signupCustomer, updateCustomerData } from '../reducers/ActionCreators/Customer';
 
 import useManageCart from './useManageCart';
 
+export interface CustomerPersonalData {
+  email: string;
+  firstName?: string;
+  lastName?: string;
+  dateOfBirth?: string;
+}
+
 export default function useManageCustomer() {
+  const { customer } = useAppSelector((state) => state.customerReducer);
   const dispatch = useAppDispatch();
   const { getAnonCartResourceIdentifier } = useManageCart();
+
+  function createCustomerUpdate(values: CustomerPersonalData) {
+    if (!customer) throw new Error('No customer!');
+
+    const updates: MyCustomerUpdate = { version: customer.version, actions: [] };
+    if (customer.firstName !== values.firstName) {
+      updates.actions.push({
+        firstName: values.firstName,
+        action: 'setFirstName',
+      });
+    }
+    if (customer.lastName !== values.lastName) {
+      updates.actions.push({
+        lastName: values.lastName,
+        action: 'setLastName',
+      });
+    }
+    if (customer.email !== values.email) {
+      updates.actions.push({
+        email: values.email,
+        action: 'changeEmail',
+      });
+    }
+    if (customer.dateOfBirth !== values.dateOfBirth) {
+      updates.actions.push({
+        dateOfBirth: values.dateOfBirth,
+        action: 'setDateOfBirth',
+      });
+    }
+
+    return updates;
+  }
+
+  function updatePersonalData(customerPersonalData: CustomerPersonalData) {
+    const updates = createCustomerUpdate(customerPersonalData);
+    if (updates.actions.length === 0) return Promise.reject(new Error('Nothing to change!'));
+    return dispatch(updateCustomerData(updates));
+  }
 
   function login(customerSignin: CustomerSignin) {
     const anonymousCart = getAnonCartResourceIdentifier();
@@ -18,5 +64,5 @@ export default function useManageCustomer() {
     return dispatch(signupCustomer({ ...customerDraft, anonymousCart }));
   }
 
-  return { login, signup };
+  return { customer, login, signup, updatePersonalData };
 }
