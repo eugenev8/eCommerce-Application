d9d560c4839c9a6d33f98cdd5c25e622225e4713
Zhenya Vasilev <vasilevz195@gmail.com>
2023-09-17 18:37:03 +0600

    feat: use new components in cart

--- src/pages/basket/Basket.tsx
+++ src/pages/basket/Basket.tsx
@@ -3,115 +3,133 @@ import { LineItem } from '@commercetools/platform-sdk';
 import Wrapper from '../../components/wrapper/Wrapper';
 import styles from './basket.module.scss';
 import useManageCart from '../../hooks/useManageCart';
-import renderLineItems from './renderLineItems';
-
-const clearCartHandler = () => {
-  console.log('clear cart');
-};
-const updateCartHandler = () => {
-  console.log('update cart');
-};
-
-const applyPromoHandler = () => {
-  console.log('apply promo');
-};
-const proceedToCheckoutHandler = () => {
-  console.log('proceed to checkout');
-};
-
-const checkoutWithPayPalHandler = () => {
-  console.log('checkout with paypal');
-};
-
-const checkoutWithMultipleAddressesHandler = () => {
-  console.log('checkout with multiple addresses');
-};
+import AnimatedContainer from '../../components/containers/AnimatedContainer';
+import ColoredContainer from '../../components/containers/ColoredContainer';
+import DiscountInput from '../../components/forms/inputs/DiscountInput';
+import Button from '../../components/buttons/Buttons';
+import FlexContainer from '../../components/containers/FlexContainer';
+import ROUTES_PATHS from '../../routesPaths';
+import QuantityInput from '../../components/forms/inputs/QuantityInput';
 
 export default function BasketPage() {
   const { cart } = useManageCart();
-  const lineItems: LineItem[] = cart?.lineItems ?? [];
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const navigate = useNavigate();
-  return (
-    <Wrapper>
-      <h2>Shopping Cart</h2>
-      <div className={styles.container}>
-        <div className={styles.container__items}>
-          <div className={styles.items__header}>
-            <div>Item</div>
-            <div />
-            <div>Price</div>
-            <div>Qty</div>
-            <div>Subtotal</div>
-            <div />
+  const lineItems: LineItem[] = cart?.lineItems ?? [];
+
+  const clearCart = () => {
+    // Shoud clear cart
+  };
+
+  let totalPrice = '0';
+  if (cart && cart.totalPrice) {
+    totalPrice = (cart.totalPrice.centAmount / 100).toFixed(cart.totalPrice.fractionDigits);
+  }
+
+  function lineItem(item: LineItem) {
+    const itemPrice = item.price.value.centAmount / 10 ** item.price.value.fractionDigits;
+    const itemAttributes = item.variant?.attributes?.map((a) => <p key={a.name}>{`${a.name}: ${a.value}`}</p>);
+
+    return (
+      <div key={item.id} className={styles.cart__item}>
+        <div className={styles.item__info}>
+          <div className={styles.item__image}>
+            <img src={item?.variant?.images?.[0]?.url} alt={item.name[`en-US`]} />
           </div>
-          {renderLineItems(lineItems)}
-          <div className={styles.cartControls}>
-            <div className={styles.leftButtons}>
-              <Link to="/catalog">
-                <button className={`${styles.buttonGray} ${styles.buttonBlackGray}`} type="button">
-                  Continue Shopping
-                </button>
-              </Link>
-              <button
-                className={`${styles.buttonBlack} ${styles.buttonBlackGray}`}
-                type="button"
-                onClick={() => clearCartHandler()}
-              >
-                Clear Shopping Cart
-              </button>
+          <div className={styles.item__productInfo}>
+            <div className={styles.item__name}>
+              <Link to={`/product/${item.productKey}?variant=${item.variant?.id}`}>{item.name[`en-US`]}</Link>
             </div>
-            <div className={styles.rightButtons}>
-              <button
-                className={`${styles.buttonBlack} ${styles.buttonBlackGray}`}
-                type="button"
-                onClick={() => updateCartHandler()}
-              >
-                Update Shopping Cart
-              </button>
-            </div>
+            <div className={styles.item__attributes}>{itemAttributes}</div>
           </div>
         </div>
-        <div className={styles.summary}>
-          <h3>Summary</h3>
-          <p>Estimate Shipping and Tax</p>
-          <p className={styles.summary__destination}>Enter your destination to get a shipping estimate.</p>
-          <div className={styles.summary__promo}>
-            <p>Apply Discount Code</p>
-            <input type="text" name="promo" id="promo" placeholder="Enter discount code" />
-            <button type="button" onClick={() => applyPromoHandler()}>
-              Apply
-            </button>
+        <div className={styles.item_inCartInfo}>
+          <div className={styles.item__quantity}>
+            <QuantityInput quantity={item.quantity} addItem={() => {}} removeItem={() => {}} />
           </div>
-          <div className={styles.summary__calculations}>
-            <div>Subtotal</div>
-            <div>$1299.00</div>
-            <div>Shipping</div>
-            <div>$21.00</div>
-            <div>Tax</div>
-            <div>$1.50</div>
-            <div>GST(10%)</div>
-            <div>$1.50</div>
-            <div>Order Total</div>
-            <div>$1349.00</div>
-          </div>
-          <div className={styles.summary__buttons}>
-            <button type="button" className={styles.summary__button} onClick={() => proceedToCheckoutHandler()}>
-              Proceed to Checkout
-            </button>
-            <button type="button" className={styles.summary__button} onClick={() => checkoutWithPayPalHandler()}>
-              Checkout with PayPal
-            </button>
-            <button
-              type="button"
-              className={styles.summary__button}
-              onClick={() => checkoutWithMultipleAddressesHandler()}
-            >
-              Checkout with multiple addresses
-            </button>
-          </div>
+          <div className={styles.item__subtotal}>{`$${(itemPrice * item.quantity).toFixed(2)}`}</div>
         </div>
       </div>
-    </Wrapper>
+    );
+  }
+
+  if (!cart || !lineItems.length) {
+    return (
+      <AnimatedContainer>
+        <Wrapper>
+          <h2>Shopping Cart</h2>
+          <FlexContainer
+            style={{
+              gap: '1rem',
+              justifyContent: 'center',
+              alignItems: 'center',
+              minHeight: '40vh',
+              flexDirection: 'column',
+            }}
+          >
+            <p>No items in cart!</p>
+            <Link to={ROUTES_PATHS.catalog}>Go to Catalog</Link>
+          </FlexContainer>
+        </Wrapper>
+      </AnimatedContainer>
+    );
+  }
+
+  return (
+    <AnimatedContainer>
+      <Wrapper>
+        <h2>Shopping Cart</h2>
+        <FlexContainer style={{ gap: '1rem' }}>
+          <Button
+            addedClass=""
+            innerText="Clear cart"
+            styling="secondary"
+            type="button"
+            variant="default"
+            style={{ margin: '1rem 0', padding: '0.5rem 1rem', minWidth: 'auto', fontSize: '.9rem' }}
+            onClick={clearCart}
+          />
+          <Button
+            addedClass=""
+            innerText="Continue shopping"
+            styling="secondary"
+            type="button"
+            variant="default"
+            style={{ margin: '1rem 0', padding: '0.5rem 1rem', minWidth: 'auto', fontSize: '.9rem' }}
+            onClick={() => {
+              navigate(ROUTES_PATHS.catalog);
+            }}
+          />
+        </FlexContainer>
+        <section className={styles.cartContainer}>
+          <div className={styles.cart}>{lineItems.map((item) => lineItem(item))}</div>
+          <div className={styles.summary}>
+            <ColoredContainer>
+              <h4>Order Summary</h4>
+              <div className={styles.promocode}>
+                <DiscountInput />
+              </div>
+              <div className={styles.orderInfo}>
+                <div className={styles.orderInfo__line}>
+                  <p>Items quantity</p>
+                  <p>{cart?.totalLineItemQuantity}</p>
+                </div>
+                <div className={styles.orderInfo__line}>
+                  <p>Total price</p>
+                  <p>${totalPrice}</p>
+                </div>
+              </div>
+              <Button
+                addedClass=""
+                innerText="Procceed to Checkout"
+                styling="primary"
+                type="button"
+                variant="default"
+                style={{ margin: '3rem auto' }}
+              />
+            </ColoredContainer>
+          </div>
+        </section>
+      </Wrapper>
+    </AnimatedContainer>
   );
 }
