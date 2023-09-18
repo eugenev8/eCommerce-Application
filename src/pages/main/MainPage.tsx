4e2994d953ec013e9e95a1b8185545265e3efd6d
Zhenya Vasilev <vasilevz195@gmail.com>
2023-09-18 17:32:33 +0600

    feat: remove old links

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,48 +1,28 @@
-import { useState } from 'react';
-import { NavLink } from 'react-router-dom';
 import styles from './MainPage.module.scss';
 
 import AnimatedContainer from '../../components/containers/AnimatedContainer';
-import toaster from '../../services/toaster';
-import useManageCart from '../../hooks/useManageCart';
+import Wrapper from '../../components/wrapper/Wrapper';
+import ColoredContainer from '../../components/containers/ColoredContainer';
 
 function MainPage() {
-  const { applyPromoCode } = useManageCart();
-  const [promoCode, setPromoCode] = useState<string>('');
-  function handleApplyPromoCode() {
-    applyPromoCode(promoCode).then((isSuccess) => {
-      if (isSuccess) {
-        toaster.showSuccess('Success!');
-      }
-    });
-  }
-
   return (
     <div className={`${styles.mainPage}`}>
       <AnimatedContainer>
-        <h1>Main page</h1>
-        <NavLink to="/">Main (you are here)</NavLink>
-        <br />
-        <NavLink to="/login">Login</NavLink>
-        <br />
-        <NavLink to="/register">Sign up</NavLink>
-        <br />
-        <br />
-        <button type="button" onClick={handleApplyPromoCode}>
-          Apply promo-code
-        </button>
-        <br />
-        <input
-          type="text"
-          value={promoCode}
-          onChange={(e) => setPromoCode(e.target.value)}
-          placeholder="promo-code"
-          style={{ width: '200px' }}
-        />
-        <p>
-          Есть промокод demo9 которые делает скидку на все товары в корзине на 5% за исключением товаров, у которых уже
-          есть скидка
-        </p>
+        <Wrapper>
+          <h2>Current promocodes</h2>
+          <ColoredContainer
+            style={{
+              borderRadius: '10px',
+              boxShadow: 'rgba(0, 0, 0, 0.1) 0px 4px 6px -1px, rgba(0, 0, 0, 0.06) 0px 2px 4px -1px',
+            }}
+          >
+            <h3>demo9</h3>
+            <p>
+              Promo code that reduces the price of all products in the cart by 5% except for products that have been
+              already discounted.
+            </p>
+          </ColoredContainer>
+        </Wrapper>
       </AnimatedContainer>
     </div>
   );
