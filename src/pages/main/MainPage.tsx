5d61577d4e25d085ca48717d745f138543b82c78
Zhenya Vasilev <vasilevz195@gmail.com>
2023-09-18 20:06:38 +0600

    feat: add new discount codes

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -14,6 +14,7 @@ function MainPage() {
             style={{
               borderRadius: '10px',
               boxShadow: 'rgba(0, 0, 0, 0.1) 0px 4px 6px -1px, rgba(0, 0, 0, 0.06) 0px 2px 4px -1px',
+              margin: '3rem auto',
             }}
           >
             <h3>demo9</h3>
@@ -21,6 +22,26 @@ function MainPage() {
               Promo code that reduces the price of all products in the cart by 5% except for products that have been
               already discounted.
             </p>
+          </ColoredContainer>
+          <ColoredContainer
+            style={{
+              borderRadius: '10px',
+              boxShadow: 'rgba(0, 0, 0, 0.1) 0px 4px 6px -1px, rgba(0, 0, 0, 0.06) 0px 2px 4px -1px',
+              margin: '3rem auto',
+            }}
+          >
+            <h3>all5</h3>
+            <p>Promo code that reduces the price of all products in the cart by 5%.</p>
+          </ColoredContainer>
+          <ColoredContainer
+            style={{
+              borderRadius: '10px',
+              boxShadow: 'rgba(0, 0, 0, 0.1) 0px 4px 6px -1px, rgba(0, 0, 0, 0.06) 0px 2px 4px -1px',
+              margin: '3rem auto',
+            }}
+          >
+            <h3>apple6</h3>
+            <p>Promo code that reduces the price of all Apple products in the cart by 6%.</p>
           </ColoredContainer>
         </Wrapper>
       </AnimatedContainer>
