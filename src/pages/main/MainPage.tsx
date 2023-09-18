89fd6fbcbc201cb6f9acceea03ce0056d7a64049
Zhenya <97748949+eugenev8@users.noreply.github.com>
2023-09-19 02:28:00 +0600

    feat: restyle_main_page (#69)
    * feat: remove old links
    
    * feat: add new discount codes
    
    * feat: replaced demo promo with a new one
    
    ---------
    
    Co-authored-by: Trachenko Viktor <trachenko.v.a@gmail.com>

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,48 +1,47 @@
-import { useState } from 'react';
-import { NavLink } from 'react-router-dom';
 import styles from './MainPage.module.scss';
 
 import AnimatedContainer from '../../components/containers/AnimatedContainer';
-import toaster from '../../services/toaster';
-import useManageCart from '../../hooks/useManageCart';
+import Wrapper from '../../components/wrapper/Wrapper';
+import ColoredContainer from '../../components/containers/ColoredContainer';
 
 function MainPage() {
-  const { applyPromoCode } = useManageCart();
-  const [promoCode, setPromoCode] = useState<string>('');
-  function handleApplyPromoCode() {
-    applyPromoCode(promoCode).then((isSuccess) => {
-      if (isSuccess) {
-        toaster.showSuccess('Success!');
-      }
-    });
-  }
-
   return (
     <div className={`${styles.mainPage}`}>
       <AnimatedContainer>
-        <h1>Main page</h1>
-        <NavLink to="/">Main (you are here)</NavLink>
-        <br />
-        <NavLink to="/login">Login</NavLink>
-        <br />
-        <NavLink to="/register">Sign up</NavLink>
-        <br />
-        <br />
-        <button type="button" onClick={handleApplyPromoCode}>
-          Apply promo-code
-        </button>
-        <br />
-        <input
-          type="text"
-          value={promoCode}
-          onChange={(e) => setPromoCode(e.target.value)}
-          placeholder="promo-code"
-          style={{ width: '200px' }}
-        />
-        <p>
-          Есть промокод demo9 которые делает скидку на все товары в корзине на 5% за исключением товаров, у которых уже
-          есть скидка
-        </p>
+        <Wrapper>
+          <h2>Current promo codes</h2>
+          <p>All promo codes work on non-discounted items.</p>
+          <ColoredContainer
+            style={{
+              borderRadius: '10px',
+              boxShadow: 'rgba(0, 0, 0, 0.1) 0px 4px 6px -1px, rgba(0, 0, 0, 0.06) 0px 2px 4px -1px',
+              margin: '3rem auto',
+            }}
+          >
+            <h3>all5</h3>
+            <p>Promo code that reduces the price of all products in the cart by 5%.</p>
+          </ColoredContainer>
+          <ColoredContainer
+            style={{
+              borderRadius: '10px',
+              boxShadow: 'rgba(0, 0, 0, 0.1) 0px 4px 6px -1px, rgba(0, 0, 0, 0.06) 0px 2px 4px -1px',
+              margin: '3rem auto',
+            }}
+          >
+            <h3>apple6</h3>
+            <p>Promo code that reduces the price of all Apple products in the cart by 6%.</p>
+          </ColoredContainer>
+          <ColoredContainer
+            style={{
+              borderRadius: '10px',
+              boxShadow: 'rgba(0, 0, 0, 0.1) 0px 4px 6px -1px, rgba(0, 0, 0, 0.06) 0px 2px 4px -1px',
+              margin: '3rem auto',
+            }}
+          >
+            <h3>win10-sale</h3>
+            <p>Promo code that reduces the price of all products with OS Windows 10 in the cart by 10%.</p>
+          </ColoredContainer>
+        </Wrapper>
       </AnimatedContainer>
     </div>
   );
