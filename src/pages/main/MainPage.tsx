b5586fde68423afe6a5e555294f6142c6f69142a
Trachenko Viktor <trachenko.v.a@gmail.com>
2023-09-18 23:26:24 +0300

    feat: replaced demo promo with a new one

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -9,20 +9,8 @@ function MainPage() {
     <div className={`${styles.mainPage}`}>
       <AnimatedContainer>
         <Wrapper>
-          <h2>Current promocodes</h2>
-          <ColoredContainer
-            style={{
-              borderRadius: '10px',
-              boxShadow: 'rgba(0, 0, 0, 0.1) 0px 4px 6px -1px, rgba(0, 0, 0, 0.06) 0px 2px 4px -1px',
-              margin: '3rem auto',
-            }}
-          >
-            <h3>demo9</h3>
-            <p>
-              Promo code that reduces the price of all products in the cart by 5% except for products that have been
-              already discounted.
-            </p>
-          </ColoredContainer>
+          <h2>Current promo codes</h2>
+          <p>All promo codes work on non-discounted items.</p>
           <ColoredContainer
             style={{
               borderRadius: '10px',
@@ -42,6 +30,16 @@ function MainPage() {
           >
             <h3>apple6</h3>
             <p>Promo code that reduces the price of all Apple products in the cart by 6%.</p>
+          </ColoredContainer>
+          <ColoredContainer
+            style={{
+              borderRadius: '10px',
+              boxShadow: 'rgba(0, 0, 0, 0.1) 0px 4px 6px -1px, rgba(0, 0, 0, 0.06) 0px 2px 4px -1px',
+              margin: '3rem auto',
+            }}
+          >
+            <h3>win10-sale</h3>
+            <p>Promo code that reduces the price of all products with OS Windows 10 in the cart by 10%.</p>
           </ColoredContainer>
         </Wrapper>
       </AnimatedContainer>
